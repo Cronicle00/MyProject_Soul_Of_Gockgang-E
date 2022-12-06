304177aa9df407dd94980cc4427d164ef3d24eea
Cronicle00 <ben010879@gmail.com>
2022-12-06 13:22:00 +0900

    map1

--- Soul_Of_Gockgang-E/Assets/02.Scripts/Go_Next.cs
+++ Soul_Of_Gockgang-E/Assets/02.Scripts/Go_Next.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Go_Next : MonoBehaviour
 {
-    public int nS;
+    public string nS;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +21,7 @@ public class Go_Next : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("OutSideHome");
+            SceneManager.LoadScene(nS);
 
         }
     }
